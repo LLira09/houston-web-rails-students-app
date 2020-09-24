19a3b3c6529044031225974787eb36d72baca6ae
github username <github email address>
2020-09-24 15:19:08 -0500

    Done.

---
+++ app/controllers/instructors_controller.rb
@@ -0,0 +1,39 @@
+class InstructorsController < ApplicationController
+    def index
+        @instructors = Instructor.all
+    end
+    def show
+        @instructor = Instructor.find(params[:id])
+    end
+
+    def new
+        @instructor = Instructor.new
+    end
+
+    def create
+        instructor = Instructor.create(instructor_params)
+        redirect_to instructor
+    end
+
+    def edit
+        @instructor = Instructor.find(params[:id])
+    end
+
+    def update
+        @instructor = Instructor.find(params[:id])
+        @instructor.update(instructor_params)
+        redirect_to @instructor
+    end
+
+    def destroy
+        @instructor = Instructor.find(params[:id])
+        @instructor.destroy
+        redirect_to instructors_path
+    end
+
+    private
+
+    def instructor_params
+        params.require(:instructor).permit(:name)
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Instructor.create(name: "Raul")
+Instructor.create(name: "Joe")
+Instructor.create(name: "Antonio")
+
+Student.create(name: "Jake", major: "Graphic Design", age: 29, instructor_id: 1)
+Student.create(name: "Haley", major: "Rails", age: 26, instructor_id: 2)
+Student.create(name: "Walid", major: "ROR", age: 25, instructor_id: 3)
+Student.create(name: "Frank", major: "Design", age: 24, instructor_id: 3)
+Student.create(name: "Payton", major: "JS", age: 26, instructor_id: 2)
